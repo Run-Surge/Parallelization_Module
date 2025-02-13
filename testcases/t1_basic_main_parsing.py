5fc3c95a287a93e9b906821db5080a7b110c8993
Omar Said <osazizsg1@gmail.com>
2025-02-14 01:08:21 +0200

    🚧 Add basic entry point support

---
+++ testcases/t1_basic_main_parsing.py
@@ -0,0 +1,19 @@
+def func1(x):
+    return x + 1
+
+def func2(x):
+    pass
+if __name__ == '__main__':
+    x = 5
+    y = x + 3
+    z = x * y
+    x = x + 2
+    y = x
+    if z > x:
+        z=9
+    elif z < y:
+        z=7
+    else:
+        z=8
+    l = func1(x)
+    func2(l)
