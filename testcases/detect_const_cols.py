c4ec3b7c5980c429ac2e8375d49dc23d63a8972c
Omar Said <osazizsg1@gmail.com>
2025-06-22 22:07:35 +0300

    fix testcases

--- testcases/detect_const_cols.py
+++ testcases/detect_const_cols.py
@@ -1,6 +1,6 @@
 ##########################################################################################################
 #! User defined variables
-
+import csv
 FILE_NAME = 'test.csv'  # Name of the file to read data from
 
 ##########################################################################################################
@@ -51,10 +51,18 @@ def detect_constant_columns(data):
 if __name__ == '__main__':
 #---------------------------------------------------------------------------------------------------------
 #! This block handles data loading please don't edit it (Note:The data is loaded into a list of name data)
+    def infer_type(value):
+        try:
+            return int(value)
+        except ValueError:
+            try:
+                return float(value)
+            except ValueError:
+                return value.strip()
     try:
         with open(FILE_NAME, 'r') as file:
             lines = file.readlines()
-            data = [line.strip().split(',') for line in lines]
+            data = [[infer_type(cell) for cell in line.strip().split(',')] for line in lines]
     except FileNotFoundError:
         print("File not found. Please ensure 'test.csv' exists in the current directory.")
 #---------------------------------------------------------------------------------------------------------
@@ -68,9 +76,10 @@ if __name__ == '__main__':
 #---------------------------------------------------------------------------------------------------------
 #! Saving the output to a file please don't edit this block
 #! output name should be a list named output
-    with open('output.csv', 'w') as file:
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
         for row in output:
-            file.write(','.join(row) + '\n')
+            writer.writerow(row)
 #---------------------------------------------------------------------------------------------------------
 
 
